f91e2e9ce7f3b41c2c985d04d9ce6a1dc653fc2a
brvhm <brahim.idt1@gmail.com>
2022-03-13 00:47:15 +0100

    List + daily tasks

--- app/src/main/java/com/example/todolist_ib/MainActivity.java
+++ app/src/main/java/com/example/todolist_ib/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.todolist_ib;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView maListe;
+    String daily[] = { "Sacar al perro", "comprar el pan",
+            "revisar el correo de la salle", "preparar reuniones del día",
+            "hacer ejercicio." };
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        maListe = findViewById(R.id.list);
+        ArrayAdapter<String> myarray;
+        myarray = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, daily);
+        maListe.setAdapter(myarray);
     }
 }
+
