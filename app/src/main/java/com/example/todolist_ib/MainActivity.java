1c93466663f4d9d0283a41c8311e19104195aaca
brvhm <brahim.idt1@gmail.com>
2022-03-13 01:52:55 +0100

    Realisado / Pediente

--- app/src/main/java/com/example/todolist_ib/MainActivity.java
+++ app/src/main/java/com/example/todolist_ib/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.todolist_ib;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView maListe;
-    String daily[] = { "Sacar al perro", "comprar el pan",
-            "revisar el correo de la salle", "preparar reuniones del día",
-            "hacer ejercicio." };
+    String daily[] = { "Sacar al perro ; pediente", "comprar el pan ; pediente",
+            "revisar el correo de la salle ; pediente", "preparar reuniones del día ; pediente",
+            "hacer ejercicio ; pediente" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +23,30 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> myarray;
         myarray = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, daily);
         maListe.setAdapter(myarray);
+        maListe.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String) parent.getItemAtPosition(position);
+                String test = item.toString();
+                String[] parties = test.split(";");
+                String itBug = parties[1];
+                if(itBug.equals(" realisado")){
+                    daily[position] = parties[0]+"; pediente";
+                }else{
+                    daily[position] = parties[0]+"; realisado";
+                }
+                    myarray.notifyDataSetChanged();
+
+
+            }
+        });
+
+    }
+
+
+    public void add(){
+
     }
 }
+
 
